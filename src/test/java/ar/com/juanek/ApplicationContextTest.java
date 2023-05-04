b0736c7731e2e40220693eafe9b3148d6aa7787f
juan <juanekipes@gmail.com>
2023-05-04 14:43:50 -0300

    junit version updated

--- src/test/java/ar/com/juanek/ApplicationContextTest.java
+++ src/test/java/ar/com/juanek/ApplicationContextTest.java
@@ -1,14 +1,15 @@
 package ar.com.juanek;
 
 import ar.com.juanek.config.ConfigTest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = ConfigTest.class)
 public class ApplicationContextTest {
 
